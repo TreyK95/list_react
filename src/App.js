df18e06a4b66bb9fe3bc5a64a16dceaae657f06a
Trey Kemp <kemptrey@outlook.com>
2022-02-27 13:03:22 -0700

    added comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  //state = a plain JS object, in this case an array of objects
   state = {
     todos: [
       { id: 1, name: "Learn Rails", complete: true },
@@ -9,9 +10,10 @@ class App extends Component {
       { id: 1, name: "Graduate DPL", complete: false },
     ],
   };
-
+  //a function we create that lets us loop through the todos inside state. We can then return JSX for each todo
   renderTodos = () => {
     const { todos } = this.state;
+    //key is a special attribute that is needed by React each elements needs a key that is unique ot that loop
     return todos.map((todo) => <li key={todo.id}>{todo.name}</li>);
   };
 
