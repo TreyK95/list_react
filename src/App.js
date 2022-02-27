6f6078004c9914b0cd6e04e4064ef0e85f800638
Trey Kemp <kemptrey@outlook.com>
2022-02-27 12:59:49 -0700

    rendering todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, name: "Learn Rails", complete: true },
+      { id: 2, name: "Learn React", complete: false },
+      { id: 1, name: "Graduate DPL", complete: false },
+    ],
+  };
+
+  renderTodos = () => {
+    const { todos } = this.state;
+    return todos.map((todo) => <li key={todo.id}>{todo.name}</li>);
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>{this.renderTodos()}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
