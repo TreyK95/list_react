582117029c54697fc890d15fa23bf38b39df69c4
Trey Kemp <kemptrey@outlook.com>
2022-02-28 20:07:07 -0700

    added task

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     todos: [
       { id: 1, name: "Learn Rails", complete: true },
       { id: 2, name: "Learn React", complete: false },
-      { id: 1, name: "Graduate DPL", complete: false },
+      { id: 3, name: "Graduate DPL", complete: false },
+      { id: 4, name: "Get a job", complete: false}.
     ],
   };
   //a function we create that lets us loop through the todos inside state. We can then return JSX for each todo
